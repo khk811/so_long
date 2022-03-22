bbe206a6c42fef9cffb7be979259ba47a090a327
khk811 <hyunkyungkim811@gmail.com>
2022-03-22 20:28:31 +0900

    check map components

--- so_long/map.c
+++ so_long/map.c
@@ -21,7 +21,7 @@ int valid_map_open(const char *dir)
     return (ret_fd);
 }
 
-int is_char_map_compo(char c)
+int is_char_map_component(char c)
 {
     if (c == '1' || c == 'E' || c == '0' \
     || c == 'P' || c == 'C')
@@ -29,15 +29,21 @@ int is_char_map_compo(char c)
     return (0);
 }
 
-int is_str_map_compo(char *s)
+int count_map_component(char *s, t_map **map)
 {
     int i;
 
     i = 0;
     while (s[i])
     {
-        if (!is_char_map_compo(s[i]))
+        if (!is_char_map_component(s[i]))
             break ;
+        if (s[i] == 'P')
+            (*map)->player_num++;
+        else if (s[i] == 'E')
+            (*map)->exit_num++;
+        else if (s[i] == 'C')
+            (*map)->item_num++;
         i++;
     }
     return (i);
@@ -52,16 +58,25 @@ int count_map_line(int map_fd, t_map **map)
 // fd 판별은 한번만 햇으면 좋겠음. 여기에서 판별하지 않았으면.
     if (map_fd < 0)
         return (0);
+    // 맵 구조체 init을 함수에서 빼서 따로 만들어야 함 (왜 여기서 init을 하는거니;;).
     map_line = get_next_line(map_fd);
     map_row = 0;
-    map_col = is_str_map_compo(map_line);
+    map_col = count_map_component(map_line, map);
+    (*map)->player_num = 0;
+    (*map)->exit_num = 0;
+    (*map)->item_num = 0;
     while (map_line)
     {
         map_row++;
-        if (is_str_map_compo(map_line) != map_col)
+        if (count_map_component(map_line, map) != map_col)
             return (0);
         map_line = get_next_line(map_fd);
     }
+    free(map_line);
+    map_line = NULL;
+    // 밑 부분을 따로 빼는게 좋을까? 아니면 그냥 둘까?
+    if ((*map)->player_num != 1 || (*map)->exit_num != 1 || (*map)->item_num < 1)
+        return (0);
     (*map)->row = map_row;
     (*map)->col = map_col;
     return (1);
@@ -99,11 +114,21 @@ void    assign_map_arr(int map_fd, t_map **map)
     {
         gnl_buf = (const char *)get_next_line(map_fd);
         ft_strlcpy((*map)->map_coord[i], gnl_buf, (*map)->col + 1);
+        // free UTIL 만들기
         free((char *)gnl_buf);
         gnl_buf = NULL;
         i++;
     }
 }
+
+/*
+int is_map_wall_covered(char **map_arr)
+{
+    // map 가로 세로 길이에 맞춰서 검사.
+    // 첫줄, 마지막줄 1만
+    // 나머지는 앞, 뒤만;
+    return (1);
+}*/
 
 int main(int argc, char **argv)
 {
@@ -116,6 +141,8 @@ int main(int argc, char **argv)
         return (0);
     the_map = malloc(sizeof(t_map));
     map_fd = valid_map_open((const char *)argv[1]);
+    if (map_fd < 0)
+        return (0);
     ret_flag = count_map_line(map_fd, &the_map);
     if (ret_flag == 1)
     {
@@ -136,6 +163,8 @@ int main(int argc, char **argv)
     }
     printf("coord(2, 3): %c\n", the_map->map_coord[2][3]);
     printf("coord(1, 1): %c\n", the_map->map_coord[1][1]);
+    printf("total player num: %d\n", the_map->player_num);
+    printf("total exit num: %d\n", the_map->exit_num);
     close(map_fd);
     return (0);
 }
