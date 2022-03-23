419e22bb07c4e47bdee378b7d4fbd1df65246ae9
khk811 <hyunkyungkim811@gmail.com>
2022-03-23 20:43:24 +0900

    add map_init

--- so_long/map.c
+++ so_long/map.c
@@ -5,20 +5,32 @@
 #include "so_long.h"
 
 // argv를 map main 함수 앞에서 거르는데 좋을듯.
-// valid_map_open 이거 하지 말고 그냥 open, close 이렇게.
 
 int valid_map_open(const char *dir)
 {
     int ret_fd;
 
     ret_fd = -1;
-    //check ext. -> .ber
     if (!ft_strnstr(dir, ".ber", ft_strlen(dir)))
         return (-1);
     ret_fd = open(dir, O_RDONLY);
     if (ret_fd < 0)
         return (-1);
     return (ret_fd);
+}
+
+t_map   *map_init(void)
+{
+    t_map   *ret;
+
+    ret = (t_map *)malloc(sizeof(t_map));
+    ret->row = 0;
+    ret->col = 0;
+    ret->player_num = 0;
+    ret->exit_num = 0;
+    ret->item_num = 0;
+    ret->map_coord = NULL;
+    return (ret);
 }
 
 int is_char_map_component(char c)
@@ -51,25 +63,19 @@ int count_map_component(char *s, t_map **map)
 
 int count_map_line(int map_fd, t_map **map)
 {
-    int map_row;
-    int map_col;
     char    *map_line;
 
 // fd 판별은 한번만 햇으면 좋겠음. 여기에서 판별하지 않았으면.
     if (map_fd < 0)
         return (0);
-    // 맵 구조체 init을 함수에서 빼서 따로 만들어야 함 (왜 여기서 init을 하는거니;;).
     map_line = get_next_line(map_fd);
-    map_row = 0;
-    map_col = count_map_component(map_line, map);
-    (*map)->player_num = 0;
-    (*map)->exit_num = 0;
-    (*map)->item_num = 0;
+    (*map)->col = count_map_component(map_line, map);
     while (map_line)
     {
-        map_row++;
-        if (count_map_component(map_line, map) != map_col)
+        ((*map)->row)++;
+        if (count_map_component(map_line, map) != (*map)->col)
             return (0);
+        free(map_line);
         map_line = get_next_line(map_fd);
     }
     free(map_line);
@@ -77,8 +83,6 @@ int count_map_line(int map_fd, t_map **map)
     // 밑 부분을 따로 빼는게 좋을까? 아니면 그냥 둘까?
     if ((*map)->player_num != 1 || (*map)->exit_num != 1 || (*map)->item_num < 1)
         return (0);
-    (*map)->row = map_row;
-    (*map)->col = map_col;
     return (1);
 }
 
@@ -144,6 +148,21 @@ int is_map_wall_covered(t_map *map)
     }
     return (1);
 }
+/*
+void    free_map_arr(char **the_map)
+{
+    int i;
+
+    i = 0;
+    while (the_map[i])
+    {
+        free(the_map[i]);
+        the_map[i] = NULL;
+        i++;
+    }
+    free(the_map);
+    the_map = NULL;
+}*/
 
 int main(int argc, char **argv)
 {
@@ -154,23 +173,18 @@ int main(int argc, char **argv)
     ret_flag = 0;
     if (argc != 2)
         return (0);
-    the_map = malloc(sizeof(t_map));
+    the_map = map_init();
     map_fd = valid_map_open((const char *)argv[1]);
     if (map_fd < 0)
         return (0);
     ret_flag = count_map_line(map_fd, &the_map);
-    if (ret_flag == 1)
-    {
-        printf("the row of the map: %d\n", the_map->row);
-        printf("the col of the map: %d\n", the_map->col);
-        close(map_fd);
-    }
-    else
+    if (ret_flag != 1)
     {
         printf("error: the map isn't right\n");
         close(map_fd);
         return (0);
     }
+    close(map_fd);
     map_fd = valid_map_open((const char *)argv[1]);
     the_map->map_coord = alloc_map_arr(the_map);
     assign_map_arr(map_fd, &the_map);
