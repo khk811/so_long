e857c78a22669fbefbed162f7e136acc4c7c66c3
khk811 <hyunkyungkim811@gmail.com>
2022-03-28 20:26:42 +0900

    add custom error message

--- so_long/main.c
+++ so_long/main.c
@@ -6,10 +6,17 @@ int main(int argc, char **argv)
     t_map   *the_map;
 
     if (argc != 2)
+    {
+        printf("Error\n: Not enough argument\n");
         return (1);
+    }
     the_map = map_parsing((const char*)argv[1]);
     if (!the_map)
+    {
+        printf("Error\n: Map parsing failed\n");
         return (1);
+    }
+    /*
     int i = 0;
     while (i < the_map->row)
     {
@@ -20,7 +27,13 @@ int main(int argc, char **argv)
     printf("coord(1, 1): %c\n", the_map->map_coord[1][1]);
     printf("total player num: %d\n", the_map->player_num);
     printf("total exit num: %d\n", the_map->exit_num);
+    */
     if (is_map_wall_covered(the_map))
         show_mlx_win(the_map);
+    else
+    {
+        printf("Error\n: map isn't covered by walls\n");
+        return (1);
+    }
     return (0);
 }
